220d83c9441ab80694b9ddfddca23e7cd6f63fae
JohnChristian-JC <mjcbacutana@tip.edu.ph>
2024-10-16 20:20:42 +0800

    Add files via upload

---
+++ xmlData.js
@@ -0,0 +1,21 @@
+module.exports = `
+<?xml version="1.0" encoding="UTF-8"?>
+<Catalog>
+  <Product>
+    <Name> Smartphone </Name>
+    <Category> Electronics </Category>
+    <Price> 299.99 </Price>
+    <Stock> 25 </Stock>
+    <Description>This is a description of Product 1</Description>
+    <ImageURL>https://genk.mediacdn.vn/139269124445442048/2023/11/24/iphone-16-1700725619028449797524-1700785784075-1700785784549337605585.png</ImageURL>
+  </Product>
+  <Product>
+    <Name> Laptop </Name>
+    <Category> Electronics </Category>
+    <Price> 799.99 </Price>
+    <Stock> 15 </Stock>
+    <Description>This is a description of Product 2</Description>
+    <ImageURL>https://laptopmedia.com/wp-content/uploads/2023/09/acer-nitro-v-15-anv15-51-non-fingerprint-with-backlit-black-05.tif-custom-scaled-e1695938821469.jpg</ImageURL>
+  </Product>
+</Catalog>
+`;
